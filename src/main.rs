9dac29bd91e19eda8af5090cbe5349572815d8b5
yoku-2093 <singular0316work@gmail.com>
2026-03-15 02:23:04 +0900

    install macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+
+#[macroquad::main("PuyoPuyo Simulator")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+
+        draw_text("Hello, PuyoPuyo Simulator!", 20.0, 20.0, 30.0, BLACK);
+
+        next_frame().await;
+    }
 }
